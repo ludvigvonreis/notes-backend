a1aa740915c39a2929622ab56d767b5209a2ea68
Ludvig <ludvigvonreis@gmail.com>
2025-01-12 12:31:45 +0100

    update CORS configuration to use HTTPS for localhost

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(logger());
 app.use(
 	"*",
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://localhost:3000",
 		allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowHeaders: ["Content-Type", "Authorization"],
 		credentials: true,
